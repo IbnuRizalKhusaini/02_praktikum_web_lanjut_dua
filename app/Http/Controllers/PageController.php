8c143117794e6d534b92f7afd5f1557e83dd9059
IbnuRizalKhusaini <2031710004@student.polinema.ac.id>
2021-09-14 13:09:54 +0700

    first commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+        echo "Selamat Datang";
+    }
+    public function about(){
+        echo "NIM  : 2031710004 <br>";
+        echo "Nama : Ibnu Rizal Khusaini";
+    }
+    public function articles ($id=1){
+        echo "Halaman Artikel dengan Id ", $id;
+    }
+}
